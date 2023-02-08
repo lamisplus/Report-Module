4f0e74261c1e535155ce4b1ec8eadda277fcbcaf
vixxjy <ajorvictor48@gmail.com>
2023-02-08 13:27:15 +0100

    [report] HTS report added

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
 
-//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-//export const url = '/api/v1/'
- export const url =  'http://localhost:8282/api/v1/';
- export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc1ODM4MjQ4fQ.Hgbj3LezsmmeFH97Q4o1hIjVmT7b-jw3ocko9wsrz-df-5DCtPPQVGq6Z1zlc0aKv1vYPHimFSCE6bC4b1AXAw';
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'
+ //export const url =  'http://localhost:8282/api/v1/';
+ //export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc1ODM4MjQ4fQ.Hgbj3LezsmmeFH97Q4o1hIjVmT7b-jw3ocko9wsrz-df-5DCtPPQVGq6Z1zlc0aKv1vYPHimFSCE6bC4b1AXAw';
