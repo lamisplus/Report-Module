4d54b44af2dfa31963722a6462bad0d90764bc94
Emeka <ilozuechukwuemeka@yahoo.com>
2023-02-09 20:14:16 +0100

    updated HtsReportService

--- src/main/java/org/lamisplus/modules/report/service/HtsReportService.java
+++ src/main/java/org/lamisplus/modules/report/service/HtsReportService.java
@@ -1,12 +1,16 @@
 package org.lamisplus.modules.report.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.lamisplus.modules.report.domain.HtsReportDto;
 import org.lamisplus.modules.report.repository.ReportRepository;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Service
+@Slf4j
 @RequiredArgsConstructor
 public class HtsReportService {
     private final ReportRepository reportRepository;
