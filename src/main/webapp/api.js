d2bd037cc91add7462729bae17d4ddfeb2d35d69
Mathew77 <mathewadegbite@gmail.com>
2023-02-10 10:05:38 +0100

    commenting the api.js

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
-// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-// export const url = '/api/v1/'
-export const url =  'http://localhost:8383/api/v1/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc1OTk1NjU5fQ.MCR_-62Qybjd3AoxmOx6xL4abGRgoWMZj8LZNMqLo-ncPhdS6FH_rwsKdRTt4Lnzsk5YSy2kltYuXMqYujsdoQ'
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'
+// export const url =  'http://localhost:8383/api/v1/';
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc1OTk1NjU5fQ.MCR_-62Qybjd3AoxmOx6xL4abGRgoWMZj8LZNMqLo-ncPhdS6FH_rwsKdRTt4Lnzsk5YSy2kltYuXMqYujsdoQ'
