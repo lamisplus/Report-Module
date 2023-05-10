e82baca39f771806e93d1e699edbb2dfa2765150
vixxjy <ajorvictor48@gmail.com>
2023-05-10 16:32:07 +0100

    [report] updates for file name

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,8 @@
 
+/*
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
-/*
+*/
 export const url = "http://localhost:8282/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjgwMjk1NTMwfQ._5gzOgygnPPOjznCw-WbrtlTXaIXt0N5DZs544a789cyrXK_oDRLZ5rsoWzK7MJdcX8G6l4TASzmKfGkSidMuQ";
-*/
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjgzNzQ3MTA5fQ.OJk9aYtVMS9vAix5hw411TkGDRWpbvGPT5tDNQCpb-hEPyZtRYzwWn00WNTcZR845KFPDby6WP_ICflETkt4Zw";
