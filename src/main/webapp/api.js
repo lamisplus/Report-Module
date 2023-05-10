5c766e3025a32aeaa200f7104538b3d7188ad5ba
vixxjy <ajorvictor48@gmail.com>
2023-05-10 17:15:07 +0100

    Fix issue with file name

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,7 @@
-
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
 /*
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/v1/'
-*/
-export const url = "http://localhost:8282/api/v1/";
+export const url = "http://localhost:9090/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjgzNzQ3MTA5fQ.OJk9aYtVMS9vAix5hw411TkGDRWpbvGPT5tDNQCpb-hEPyZtRYzwWn00WNTcZR845KFPDby6WP_ICflETkt4Zw";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjgzNzc2NjE2fQ.2Iw4Fr4trGhHaT_DgVPvSxZzIh-rQr3q8LyXGNxWQeBDJE8AlmaiKE1qJhr5-dcwOakXrp7mgBK-fvjwlbooQg";
+*/
