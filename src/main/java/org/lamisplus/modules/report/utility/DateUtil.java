2f80690683213982eb810dea3f32c0c7919cd429
Emeka <ilozuechukwuemeka@yahoo.com>
2023-06-20 16:47:35 +0100

    DateUtil added for date padding

---
+++ src/main/java/org/lamisplus/modules/report/utility/DateUtil.java
@@ -0,0 +1,23 @@
+package org.lamisplus.modules.report.utility;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class DateUtil {
+
+    public LocalDate datePadding(LocalDate date){
+        LocalDate currentDate = LocalDate.now();
+        LocalDate suppliedDate = date;
+        LOG.info("current date is {}", currentDate);
+        LOG.info("supplied date is {}", date);
+        //Add one day if current day to carter for patient registered today
+        if(currentDate.isEqual(suppliedDate)) date=date.plusDays(1);
+        return date;
+    }
+}
