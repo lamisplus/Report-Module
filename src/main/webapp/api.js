46a345f81835d58b1674c709b732515a6dc0501b
vixxjy <ajorvictor48@gmail.com>
2023-07-04 12:06:24 +0100

    [report] updates for Prep and HTS reports

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,8 @@
-/*
+
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
-*/
+/*
 export const url = "http://localhost:8383/api/v1/";
 export const token =
   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg4NDU5MzYwfQ.fCd6nd3rusX6I1nK4PbxAodkycmZsakZ7JV41a_GT1rMVrBk1dfQDBRSWWyf9u4ahpPFFBiHx0WAciGcn8a5mA";
-
+*/
