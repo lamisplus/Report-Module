749eade2e03da75eaab1b5cf1b3101e952209879
Josh_Code <rollingmoney2015@gmail.com>
2024-03-15 14:21:28 +0100

    comment tb, eac and ncd report

--- src/main/webapp/jsx/components/Reports/Index.js
+++ src/main/webapp/jsx/components/Reports/Index.js
@@ -186,44 +186,45 @@ const Reports = (props) => {
                     {/* <Label color='teal'>5</Label> */}
                   </Menu.Item>
 
-//                  <Menu.Item
-//                    name="inbox"
-//                    active={activeItem === "tb-report"}
-//                    onClick={() => handleItemClick("tb-report")}
-//                    style={{
-//                      backgroundColor: activeItem === "tb-report" ? "#000" : "",
-//                    }}
-//                  >
-//                    <span style={{ color: "#fff" }}>TB REPORT</span>
+                {/* <Menu.Item
+                   name="inbox"
+                   active={activeItem === "tb-report"}
+                   onClick={() => handleItemClick("tb-report")}
+                   style={{
+                     backgroundColor: activeItem === "tb-report" ? "#000" : "",
+                   }}
+                 >
+                   <span style={{ color: "#fff" }}>TB REPORT</span>
 
-                    {/* <Label color='teal'>5</Label> */}
-                  </Menu.Item>
-//                  <Menu.Item
-//                    name="inbox"
-//                    active={activeItem === "ncd-report"}
-//                    onClick={() => handleItemClick("ncd-report")}
-//                    style={{
-//                      backgroundColor:
-//                        activeItem === "ncd-report" ? "#000" : "",
-//                    }}
-//                  >
-//                    <span style={{ color: "#fff" }}>NCD Report</span>
+                   <Label color='teal'>5</Label>
+                 </Menu.Item> */}
+                 {/* <Menu.Item
+                   name="inbox"
+                   active={activeItem === "ncd-report"}
+                   onClick={() => handleItemClick("ncd-report")}
+                   style={{
+                     backgroundColor:
+                       activeItem === "ncd-report" ? "#000" : "",
+                   }}
+                 >
+                   <span style={{ color: "#fff" }}>NCD Report</span>
 
-                    {/* <Label color='teal'>5</Label> */}
-//                  </Menu.Item>
-//                  <Menu.Item
-//                    name="inbox"
-//                    active={activeItem === "eac-report"}
-//                    onClick={() => handleItemClick("eac-report")}
-//                    style={{
-//                      backgroundColor:
-//                        activeItem === "eac-report" ? "#000" : "",
-//                    }}
-//                  >
-//                    <span style={{ color: "#fff" }}>EAC Report</span>
+                   <Label color='teal'>5</Label>
+                 </Menu.Item> */}
+                 {/* <Menu.Item
+                   name="inbox"
+                   active={activeItem === "eac-report"}
+                   onClick={() => handleItemClick("eac-report")}
+                   style={{
+                     backgroundColor:
+                       activeItem === "eac-report" ? "#000" : "",
+                   }}
+                 >
+                   <span style={{ color: "#fff" }}>EAC Report</span>
 
-                    {/* <Label color='teal'>5</Label> */}
-                  </Menu.Item>
+                   <Label color='teal'>5</Label>
+                 </Menu.Item> */}
+
                   <Menu.Item
                     name="inbox"
                     active={activeItem === "index-elicitation"}
@@ -310,28 +311,28 @@ const Reports = (props) => {
                     completed={completed}
                   />
                 )}
-//                {activeItem === "tb-report" && (
-//                  <TbReport
-//                    handleItemClick={handleItemClick}
-//                    setCompleted={setCompleted}
-//                    completed={completed}
-//                  />
-//                )}
-//                {activeItem === "ncd-report" && (
-//                  <NcdReport
-//                    handleItemClick={handleItemClick}
-//                    setCompleted={setCompleted}
-//                    completed={completed}
-//                  />
-//                )}
-//
-//                {activeItem === "eac-report" && (
-//                  <EACReport
-//                    handleItemClick={handleItemClick}
-//                    setCompleted={setCompleted}
-//                    completed={completed}
-//                  />
-//                )}
+              {/* {activeItem === "tb-report" && (
+                 <TbReport
+                   handleItemClick={handleItemClick}
+                   setCompleted={setCompleted}
+                   completed={completed}
+                 />
+               )} */}
+               {/* {activeItem === "ncd-report" && (
+                 <NcdReport
+                   handleItemClick={handleItemClick}
+                   setCompleted={setCompleted}
+                   completed={completed}
+                 />
+               )} */}
+
+            {/* {activeItem === "eac-report" && (
+                 <EACReport
+                   handleItemClick={handleItemClick}
+                   setCompleted={setCompleted}
+                   completed={completed}
+                 />
+               )} */}
                 {activeItem === "hts-report" && (
                   <HTSReport
                     handleItemClick={handleItemClick}
