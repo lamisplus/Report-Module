70eaf4c63a62729c8946cf4e2f0d76ce7f3f2b58
Josh_Code <rollingmoney2015@gmail.com>
2024-06-14 20:43:59 +0100

    CustomReport FrontEnd Implementation

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,7 +2,7 @@ export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
 
 
-// export const url = "http://localhost:8789/api/v1/";
+// export const url = "http://localhost:8383/api/";
 // export const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3MjMzNDE2fQ.FquJ1rkUZ4W1XVLjB1-zHMS9WUAPoOFSuQFxGUDL73Ez6d1Xox3zaMAh6kilI2j0qSDpqXn-ztAB57663qottw";
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzbGltYmF0ekBnbWFpbC5jb20iLCJhdXRoIjoiU3VwZXIgQWRtaW4iLCJuYW1lIjoiIEtlbHZpbiAgSW1hcmFoIiwiZXhwIjoxNzE4NDIxNjM5fQ.XNj1hAfMuUr6OtPOa9J6L8BojvDnUpnMz6Qb4KaUfNqmfhtzJFy2nlscMaFTCFkFp7nZXX5VbOqWCJIG6SBICg";
 
--- src/main/webapp/jsx/components/Shared/Scrollable.js
+++ src/main/webapp/jsx/components/Shared/Scrollable.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import {FormGroup, Label, CardBody, Spinner,Input,Form} from "reactstrap";
+import React, {useState} from 'react';
+import {FormGroup, Label, Input} from "reactstrap";
 // import { Input } from '@material-ui/core';
 
 const ScrollableDiv = (props) => {
   const items = Array.from({ length: 50 }, (_, index) => `Item ${index + 1}`);
+  const [parameterFormFields, setParameterFormFields] = useState({});
 
+  props.onData(parameterFormFields);
+  const handleChange =(e) => {
+    const {name, value} = e.target;
+    setParameterFormFields({
+      ...parameterFormFields,
+      [name]: value,
+    });
+    props.onData(parameterFormFields);
+  };
+
+  const handleParameterSubmission =(e) => {
+    e.preventDefault();
+    props.onData(parameterFormFields);
+  };
 
   function extractPatterns(e) {
     const pattern = /\{\{([^}]+)\}\}/g;
@@ -12,6 +27,10 @@ const ScrollableDiv = (props) => {
     while ((match = pattern.exec(e)) !== null) {
       console.log(match[1]);
     }
+  }
+
+  function formatComponentName(value){
+    return value.toLowerCase().replace(" ", "_");
   }
 
   const containerStyle = {
@@ -28,55 +47,62 @@ const ScrollableDiv = (props) => {
   };
 
   const itemStyle = {
-    padding: '5px',
-    borderBottom: '1px solid #ddd'
+    padding: '2px',
+    // borderBottom: '1px solid #ddd'
   };
 
   return (
     <div style={containerStyle}>
       <div style={scrollableDivStyle}>
         {props.listOfParams?.map((match, index) => (
-          <div key={index} style={{...itemStyle, display:"flex"}}>
-            <FormGroup>
-            <Label>{Object.keys(match)[0]}</Label>
-            if({Object.keys(match)[0]}.includes("Facility")){
-              <select
-                  className="form-control"
-                  name="organisationUnitId"
-                  id="organisationUnitId"
-                  value={props.objValues?.organisationUnitId}
-                  style={{border: "1px solid #014D88", borderRadius:"0.2rem"}}>
-                  <option value={""}></option>
-                  {props.objValues?.facilities.map((value) => (
-                      <option key={value.id} value={value.organisationUnitId}>
-                          {value.organisationUnitName}
-                      </option>
-                  ))}
-              </select>
-            }else if({Object.keys(match)[0]}.includes("Date")){
-              <Input
-                type="date"
-                className="form-control"
-                name="endDate"
-                id="endDate"
-                min={"1980-01-01"}
-                max={props.objValues?.currentDate}
-                style={{border: "1px solid #014D88", borderRadius:"0.2rem"}}
-              />
-            }else{
-              <Input
-                type="date"
-                className="form-control"
-                name="endDate"
-                id="endDate"
-                min={"1980-01-01"}
-                max={props.objValues?.currentDate}
-                style={{border: "1px solid #014D88", borderRadius:"0.2rem"}}
-              />
-            }
-            </FormGroup>
+          <div key={index} style={{...itemStyle}}>
+            <form id="parameterForm" onSubmit={handleParameterSubmission}>
+              <div class="row col-md-12">
+                <div class="mb-2 col-md-4">
+                  <Label>{Object.keys(match)[0]}</Label>
+                </div>
+                <div class="mb-2 col-md-8">
+                {Object.keys(match)[0].includes("Facility") ?
+                  <select
+                      className="form-control"
+                      name={formatComponentName(Object.keys(match)[0])}
+                      id={formatComponentName(Object.keys(match)[0])}
+                      value={parameterFormFields[formatComponentName(Object.keys(match)[0])]}
+                      style={{border: "1px solid #014D88", borderRadius:"0.2rem"}}
+                      onChange={handleChange}>
+                        <option value={"0"}>Select a Facility</option>
+                          {props.facilityData.map((value) => (
+                              <option key={value.id} value={value.organisationUnitId}>
+                                  {value.organisationUnitName}
+                              </option>
+                          ))}
+                  </select> 
+                  : (Object.keys(match)[0].includes("Date") ?
+                  <Input
+                    type="date"
+                    className="form-control"
+                    name={formatComponentName(Object.keys(match)[0])}
+                    id={formatComponentName(Object.keys(match)[0])}
+                    min={"1980-01-01"}
+                    max={props.objValues?.currentDate}
+                    style={{border: "1px solid #014D88", borderRadius:"0.2rem"}}
+                    value={parameterFormFields[formatComponentName(Object.keys(match)[0])]}
+                    onChange={handleChange}
+                  /> :
+                  <Input
+                    type="text"
+                    className="form-control"
+                    name={formatComponentName(Object.keys(match)[0])}
+                    id={formatComponentName(Object.keys(match)[0])}
+                    style={{border: "1px solid #014D88", borderRadius:"0.2rem"}}
+                    value={parameterFormFields[formatComponentName(Object.keys(match)[0])]}
+                    onChange={handleChange}
+                  />
+                )}
+                </div>
+              </div>
+            </form>
           </div>
-          
           ))}
       </div>
     </div>
