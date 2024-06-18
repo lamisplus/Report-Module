ddf06d4792c337b506100e4864c5f246aa8d2939
Josh_Code <rollingmoney2015@gmail.com>
2024-06-18 21:06:51 +0100

    api remove

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,8 @@
-// export const token = new URLSearchParams(window.location.search).get("jwt");
-// export const url = "/api/v1/";
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
 
 
-export const url = "http://localhost:8383/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzbGltYmF0ekBnbWFpbC5jb20iLCJhdXRoIjoiU3VwZXIgQWRtaW4iLCJuYW1lIjoiIEtlbHZpbiAgSW1hcmFoIiwiZXhwIjoxNzE4NzU3NTEzfQ.wy3nUuGdlSP2NjplTeokxlz5BHzwwxVA4quLNtm8Sx28ffnqXewzfG3MGh5XANbhjPQvnajEU0KtPypQcOvhnQ";
+// export const url = "http://localhost:8383/api/v1/";
+// export const token =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzbGltYmF0ekBnbWFpbC5jb20iLCJhdXRoIjoiU3VwZXIgQWRtaW4iLCJuYW1lIjoiIEtlbHZpbiAgSW1hcmFoIiwiZXhwIjoxNzE4NzU3NTEzfQ.wy3nUuGdlSP2NjplTeokxlz5BHzwwxVA4quLNtm8Sx28ffnqXewzfG3MGh5XANbhjPQvnajEU0KtPypQcOvhnQ";
 
