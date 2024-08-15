3ff3d952a5df49e5720271946a3ff6929daa9473
Josh_Code <rollingmoney2015@gmail.com>
2024-08-15 11:12:24 +0100

    clean up biometric report console error

--- src/main/webapp/jsx/components/Reports/BiometricReport.js
+++ src/main/webapp/jsx/components/Reports/BiometricReport.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import {FormGroup, Label , CardBody, Spinner,Input,Form} from "reactstrap";
+import {FormGroup, Label , CardBody, Input} from "reactstrap";
 import {makeStyles} from "@material-ui/core/styles";
 import {Card} from "@material-ui/core";
 // import {Link, useHistory, useLocation} from "react-router-dom";
@@ -10,7 +10,7 @@ import 'react-phone-input-2/lib/style.css'
 import { Button} from 'semantic-ui-react'
 import { toast} from "react-toastify";
 import FileSaver from "file-saver";
-import { Message, Icon } from 'semantic-ui-react'
+import { Message } from 'semantic-ui-react'
 import ProgressComponent from "./ProgressComponent"
 
 
@@ -141,7 +141,7 @@ const BiometricReport = (props) => {
           <CardBody>
             <h2 style={{ color: "#000" }}>BIOMETRIC REPORT</h2>
             <br />
-            <form >
+            < >
                         <div className="row">
                         <div className="form-group  col-md-6">
                                 <FormGroup>
@@ -234,7 +234,7 @@ const BiometricReport = (props) => {
 
                 )}
               </div>
-            </form>
+            </>
           </CardBody>
         </Card>
       </>
