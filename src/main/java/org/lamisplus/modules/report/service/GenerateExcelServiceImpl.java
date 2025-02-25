56857135920b311223c4c656b128c2101fefce7b
joshuagabriel-datafi <joshua.gabriel@thepalladiumgroup.com>
2025-02-25 09:02:05 +0100

    added Pmtct implementation

--- src/main/java/org/lamisplus/modules/report/service/GenerateExcelServiceImpl.java
+++ src/main/java/org/lamisplus/modules/report/service/GenerateExcelServiceImpl.java
@@ -280,6 +280,25 @@ public class GenerateExcelServiceImpl implements GenerateExcelService {
 		return null;
 	}
 
+	@Override
+	public ByteArrayOutputStream generatePmtctHts(Long facilityId, LocalDate start, LocalDate end, String reportType) {
+		messagingTemplate.convertAndSend(Constants.REPORT_GENERATION_PROGRESS_TOPIC, "Retrieving records from database ...");
+		LOG.info("Start generating Pmtct hts for facility:" + getFacilityName(facilityId)+ "startDate: " +start + "endDate: " +end);
+		try {
+			List<PmtctDto> pmtctHtsReport = reportRepository.getPmtctReportMetaData(facilityId, start, end);
+			LOG.error("Hts Size: {}", pmtctHtsReport.size());
+			messagingTemplate.convertAndSend(Constants.REPORT_GENERATION_PROGRESS_TOPIC, "Mapping result set ...");
+			List<Map<Integer, Object>> data = excelDataHelper.fillPmtctHtsDataMapper(pmtctHtsReport, reportType);
+			messagingTemplate.convertAndSend(Constants.REPORT_GENERATION_PROGRESS_TOPIC, "Retrieving report headers ...");
+			return excelService.generate(Constants.PMTCT_HTS_SHEET, data, Constants.PMTCT_HTS_HEADER);
+		} catch (Exception e) {
+			LOG.error("Error Occurred when generating PMTCT HTS !!!");
+			e.printStackTrace();
+		}
+		LOG.info("End generate patient PMTCT HTS");
+		return null;
+	}
+
 
 
 	@Override
