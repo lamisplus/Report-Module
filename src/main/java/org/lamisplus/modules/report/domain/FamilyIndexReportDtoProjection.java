3998a3b605c4175844bf4d22ac9523a9ee17ad4c
joshuagabriel-datafi <joshua.gabriel@thepalladiumgroup.com>
2025-03-27 11:01:32 +0100

    DFN-2558- Remove Column Index contact's name from the HTS Index report DFN-2557- No Family Index Client contact Unique Identifier on the index report

--- src/main/java/org/lamisplus/modules/report/domain/FamilyIndexReportDtoProjection.java
+++ src/main/java/org/lamisplus/modules/report/domain/FamilyIndexReportDtoProjection.java
@@ -24,7 +24,7 @@ public interface FamilyIndexReportDtoProjection {
 
     String getEntryPoint();
     Integer getElicitedAge();
-    String getElicitedClientName();
+    //String getElicitedClientName();
     String getElicitedClientSex();
     String getElicitedClientAddress();
     String getElicitedClientPhoneNumber();
