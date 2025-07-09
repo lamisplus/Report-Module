278562a987927809e73db29c2508561683f33adc
joshuagabriel-datafi <joshua.gabriel@thepalladiumgroup.com>
2025-07-09 13:21:17 +0100

    archived pharmacyRecords

--- src/main/java/org/lamisplus/modules/report/repository/queries/PHARMACYReportQuery.java
+++ src/main/java/org/lamisplus/modules/report/repository/queries/PHARMACYReportQuery.java
@@ -22,13 +22,13 @@ public class PHARMACYReportQuery {
             "    pharmacy_object ->> 'duration' AS duration,\n" +
             "    CAST(pharmacy_object ->> 'regimenName' AS VARCHAR) AS regimen_name\n" +
             "FROM hiv_art_pharmacy h,\n" +
-            "    jsonb_array_elements(h.extra->'regimens') WITH ORDINALITY p(pharmacy_object)\n" +
+            "    jsonb_array_elements(h.extra->'regimens') WITH ORDINALITY p(pharmacy_object) WHERE archived = 0\n" +
             ") AS result\n" +
             "INNER JOIN patient_person p ON p.uuid = result.person_uuid\n" +
             "LEFT JOIN ( SELECT * FROM (\n" +
             "SELECT person_uuid, dsd_model, date_devolved,\n" +
             "ROW_NUMBER()  OVER (PARTITION BY person_uuid ORDER BY date_devolved DESC) AS rnnk\n" +
-            "FROM dsd_devolvement)subQ WHERE rnnk = 1\n" +
+            "FROM dsd_devolvement WHERE archived = 0)subQ WHERE rnnk = 1\n" +
             ") dsd ON p.uuid = dsd.person_uuid\n" +
             "INNER JOIN base_organisation_unit org ON org.id = result.facility_id\n" +
             "INNER JOIN base_organisation_unit_identifier oi ON oi.organisation_unit_id = result.facility_id AND oi.name = 'DATIM_ID'\n" +
