1fa73078b2e09bdb8c40afe40bf9a69336daaa70
Joshua Gabriel <joshua.gabriel@thepalladiumgroup.com>
2025-09-16 12:48:17 +0100

    Added FACILITY_HTS_TEST_SETTING_OTHERS mapping

--- src/main/java/org/lamisplus/modules/report/repository/queries/PMTCTReportQuery.java
+++ src/main/java/org/lamisplus/modules/report/repository/queries/PMTCTReportQuery.java
@@ -64,7 +64,7 @@ public class PMTCTReportQuery {
             "WHEN hts_risk.entry_point = 'HTS_ENTRY_POINT_FACILITY' AND hts_risk.testing_setting = 'FACILITY_HTS_TEST_SETTING_WARD_INPATIENT' THEN 'Inpatient'\n" +
             "WHEN hts_risk.entry_point = 'HTS_ENTRY_POINT_FACILITY' AND hts_risk.testing_setting = 'FACILITY_HTS_TEST_SETTING_CT' THEN 'VCT'\n" +
             "WHEN hts_risk.entry_point = 'HTS_ENTRY_POINT_FACILITY' AND hts_risk.testing_setting = 'FACILITY_HTS_TEST_SETTING_TB' THEN 'TB_STAT/OtherPITC'\n" +
-            "WHEN hts_risk.entry_point = 'HTS_ENTRY_POINT_FACILITY' AND hts_risk.testing_setting IN ('FACILITY_HTS_TEST_SETTING_FP', 'FACILITY_HTS_TEST_SETTING_BLOOD_BANK', 'FACILITY_HTS_TEST_SETTING_STANDALONE_HTS','FACILITY_HTS_TEST_SETTING_OTHERS_(SPECIFY)') THEN 'Other PITC'\n" +
+            "WHEN hts_risk.entry_point = 'HTS_ENTRY_POINT_FACILITY' AND hts_risk.testing_setting IN ('FACILITY_HTS_TEST_SETTING_FP', 'FACILITY_HTS_TEST_SETTING_BLOOD_BANK', 'FACILITY_HTS_TEST_SETTING_STANDALONE_HTS','FACILITY_HTS_TEST_SETTING_OTHERS_(SPECIFY)', 'FACILITY_HTS_TEST_SETTING_OTHERS') THEN 'Other PITC'\n" +
             "WHEN hts_risk.entry_point = 'HTS_ENTRY_POINT_FACILITY' AND hts_risk.testing_setting ='FACILITY_HTS_TEST_SETTING_STI' THEN 'STI'\n" +
             "WHEN hts_risk.entry_point = 'HTS_ENTRY_POINT_FACILITY' AND hts_risk.testing_setting ='FACILITY_HTS_TEST_SETTING_SNS' THEN 'SNS'\n" +
             "WHEN hts_risk.entry_point = 'HTS_ENTRY_POINT_FACILITY' AND hts_risk.testing_setting ='FACILITY_HTS_TEST_SETTING_INDEX' THEN 'Index'\n" +
