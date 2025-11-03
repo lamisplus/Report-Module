397826a494e661037864789feb519ecd9d5ad8e4
Joshua Gabriel <joshua.gabriel@thepalladiumgroup.com>
2025-11-03 08:50:45 +0100

    rename to Previously Known HIV +Ve Status

--- src/main/java/org/lamisplus/modules/report/service/Constants.java
+++ src/main/java/org/lamisplus/modules/report/service/Constants.java
@@ -551,7 +551,7 @@ public class Constants {
 			"Date Of Maternal Retesting",
 			"Maternal Retesting Result",
 			"Mother''s ART Start Date",
-			"Previously Known HIV Status",
+			"Previously Known HIV +Ve Status",
 			"Mother''s Unique ID",
 			"Linked to Syphilis Treatment"
 	);
