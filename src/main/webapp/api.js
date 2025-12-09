572cb709cdad199d7e93eb899f2a88e53112330d
Joshua Gabriel <joshua.gabriel@thepalladiumgroup.com>
2025-12-09 16:34:43 +0100

    complete familyIndex

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,8 @@
-export const token = new URLSearchParams(window.location.search).get('jwt');
-export const url = '/api/v1/';
-export const wsUrl = '/websocket';
+// export const token = new URLSearchParams(window.location.search).get('jwt');
+// export const url = '/api/v1/';
+// export const wsUrl = '/websocket';
 
-// export const wsUrl = 'http://localhost:8383/websocket';
-// export const url = 'http://localhost:8383/api/v1/';
-// export const token =
-//   'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzU4MTIzNTM4fQ.bPRvszUlS6ZO4igmjba6roFJimO1GJ7DmZ2j02KSOxR1vT2iPTpCikxTG0jAYDW5W0NTtK2nUs-0jtoC5MSfhg';
+export const wsUrl = 'http://localhost:8383/websocket';
+export const url = 'http://localhost:8383/api/v1/';
+export const token =
+  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzY0Njk3ODY4fQ.7AmMrZYu6w2SJqGa-6dlEj6JfQDPqUQd_4fsbZ-acoXe51DxQEl1-zrWM-dwvyoBHIk1Xc-B3xwAG9iRpEAkjw';
