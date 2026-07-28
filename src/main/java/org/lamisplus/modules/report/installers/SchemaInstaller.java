c8e25190006765898a88f2ca493873eead5ad156
Joshua Gabriel <joshua.gabriel@thepalladiumgroup.com>
2026-07-28 16:00:45 +0100

    add: add months of the years to the database

--- src/main/java/org/lamisplus/modules/report/installers/SchemaInstaller.java
+++ src/main/java/org/lamisplus/modules/report/installers/SchemaInstaller.java
@@ -7,7 +7,7 @@ import org.springframework.core.annotation.Order;
 @Order(1)
 @Installer(name = "schema-installer",
         description = "Installs the required database for report tables",
-        version = 2)
+        version = 3)
 public class SchemaInstaller extends AcrossLiquibaseInstaller {
     public SchemaInstaller() {
         super("classpath:installers/report/schema.xml");
